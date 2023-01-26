dfcaf4d690b0d3110e50f91201c56ae1ddb2ff25
ngantruc2003 <ngantruc2003@gmail.com>
2023-01-27 00:30:59 +0700

    fix bug button and update styles

--- src/assets/components/buttonStyle.js
+++ src/assets/components/buttonStyle.js
@@ -4,17 +4,19 @@ const buttonStyle = theme => ({
     button: {
         ...defaultFont,
         boxShadow: "none",
-        display: "flex",
+        display: "inline-flex",
+        justifyContent: "center",
+        width: "auto",
         padding: "12px 20px 12px 20px",
         textTransform: "none",
         backgroundColor: primaryColor,
         color: whiteColor,
         borderRadius: "50px",
-        // "& :hover": {
-        //     ...cardStyle,
-        //     boxShadow: "none",
-        //     color: primaryColor + "!important",
-        // },
+        "&:hover": {
+            ...cardStyle,
+            boxShadow: "none",
+            color: primaryColor + "!important",
+        },
         [theme.breakpoints.down("sm")]: {
             fontSize: "13px",
             padding: "8px 12px 8px 12px",
